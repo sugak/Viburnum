46f4a85845b577033e1d10c776eeee35bcda3c35
Maksim Sugak <max.sugak@gmail.com>
2019-03-06 11:34:11 +0300

    Commit #4.16
    FInal release.
    Minor comments changes.

--- Viburnum/Controller/ConversationListViewController.swift
+++ Viburnum/Controller/ConversationListViewController.swift
@@ -7,20 +7,24 @@
 //
 
 /*
- === ЧЕКЛИСТ ===
- для переключения между классами Objective-C & Swift
+ 
+                          === ЧЕКЛИСТ ===
+          для переключения между классами Objective-C & Swift
  
  1. Сменить Target Membership
  2. Проверить Custom Class в IB
  3. В ConversationListViewController:
+ 
  3.1 Для перехода в Swift:
  3.1.1 В segue "themeMenu" раскомментить блок //For Swift class usage:
  3.1.2 Там же закомментить блок // For Objective-C class usage:
  3.1.3 Закомментить extension ConversationListViewController: ThemesViewControllerDelegate в конце кода
+ 
  3.2 Для перехода в Obj-C:
  3.2.1 В segue "themeMenu" закомментить блок //For Swift class usage:
  3.2.2 В segue "themeMenu" раскомментить блок // For Objective-C class usage:
  3.2.3 Раскомментить extension ConversationListViewController: ThemesViewControllerDelegate в конце кода
+ 
  */
 
 import UIKit
