d0f7fb6404b3d078b9245b8ceea142b7d90be843
Maksim Sugak <max.sugak@gmail.com>
2019-04-23 21:15:50 +0300

    Final release

--- Viburnum/RootAssembly.swift
+++ Viburnum/RootAssembly.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 
+
 //class RootAssembly {
 //  lazy var presentationAssembly: IPresentationAssembly = PresentationAssembly(serviceAssembly: self.serviceAssembly)
 //  private lazy var serviceAssembly: IServicesAssembly = ServicesAssembly(coreAssembly: self.coreAssembly)
